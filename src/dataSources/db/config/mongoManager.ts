384f7a607481678bd03593d542dbea5a08f6dd9f
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-03-30 15:44:23 -0300

    Conectando o MongoDb

--- src/dataSources/db/config/mongoManager.ts
+++ src/dataSources/db/config/mongoManager.ts
@@ -1,11 +1,19 @@
+import { MongoClient } from "mongodb";
+
 export class MongoManager {
   public static instance: MongoManager;
+  private client: MongoClient | null = null;
   private constructor() {}
 
-  public static getInstace(): MongoManager {
+  public static getInstance(): MongoManager {
     if (!MongoManager.instance) {
       MongoManager.instance = new MongoManager();
     }
     return MongoManager.instance;
+  }
+  public async connect(url: string){
+    if(!this.client){
+        this.client = await MongoClient.connect(url)
+    }
   }
 }
